26e44bb550afbe5cc929c7d4b6a86c7ee560e04e
RodL0d <Rodrigo.manchein@gmail.com>
2024-09-16 08:40:52 -0300

    initialCommit

---
+++ Assets/Scripts/Apple.cs
@@ -0,0 +1,18 @@
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Apple : MonoBehaviourPun
+{
+
+    const int speed = 5;
+    [SerializeField] int score;
+    Rigidbody2D rb;
+
+    /*public int aplleScore()
+    {
+
+    }*/
+
+}

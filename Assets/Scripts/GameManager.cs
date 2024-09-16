0cb92479e9420de13dc867344bca5c4d2a62d771
Jaspiooooon <joao14antuness@gmail.com>
2024-09-16 09:22:13 -0300

    Criação de Métodos
    Void AddPlayer, Void AddScore

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,17 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 public class GameManager : MonoBehaviourPun
 {
 
-    Vector2 ScreeenBounds;
+    Vector2 screenBounds;
     float Score;
     int playersInGame;
+    public Vector2 ScreeenBounds1 { get => screenBounds; }
+
 
     #region Singleton
 
     public static GameManager instance;
+
 
     private void Awake()
     {
@@ -25,9 +29,24 @@ public class GameManager : MonoBehaviourPun
         {
             Destroy(gameObject);
         }
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+    }
+    #endregion
+    private void Start()
+    {
+        Score = 0;
+
+    }
+    void AddPlayer()
+    {
+        playersInGame++;
+
     }
 
-    #endregion
+    void AddScore()
+    {
+        Score++;
+    }
 
 
 }
